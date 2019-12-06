3a329e75d0feacce4e80a7d0a10e5c9473310ea4
Jeff Turcotte <jeff.turcotte@gmail.com>
2019-12-06 17:05:52 -0500

    Removed PHP 5.6, Added LDAP extension, 0.12.0 Release

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -e
 OPS_VERSION=$(jq -r .version package.json)
 echo -n $OPS_VERSION > VERSION
 
-docker build -t "imarcagency/ops-apache-php56:$OPS_VERSION" images/php56
+#docker build -t "imarcagency/ops-debian-stretch-libv8:$OPS_VERSION" images/libv8
 docker build -t "imarcagency/ops-apache-php71:$OPS_VERSION" images/php71
 docker build -t "imarcagency/ops-apache-php72:$OPS_VERSION" images/php72
 docker build -t "imarcagency/ops-apache-php73:$OPS_VERSION" images/php73
