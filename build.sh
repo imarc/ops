5f8ec0c77c73125d7c43ae2d40563c42b2971642
Kevin Hamer <kevin@imarc.com>
2024-07-17 13:22:42 -0400

    uncomment all builds

--- build.sh
+++ build.sh
@@ -12,14 +12,14 @@ echo "Building docker images for $PLATFORM and ops version $OPS_VERSION..."
 
 echo -n $OPS_VERSION > VERSION
 
-#docker buildx build --platform=$PLATFORM -t "imarcagency/ops-apache-php72:$OPS_VERSION" images/php72 --progress=plain --push
-#docker buildx build --platform=$PLATFORM -t "imarcagency/ops-apache-php73:$OPS_VERSION" images/php73 --progress=plain --push
-#docker buildx build --platform=$PLATFORM -t "imarcagency/ops-apache-php74:$OPS_VERSION" images/php74 --progress=plain --push
-#docker buildx build --platform=$PLATFORM -t "imarcagency/ops-apache-php80:$OPS_VERSION" images/php80 --progress=plain --push
-#docker buildx build --platform=$PLATFORM -t "imarcagency/ops-apache-php81:$OPS_VERSION" images/php81 --progress=plain --push
-#docker buildx build --platform=$PLATFORM -t "imarcagency/ops-apache-php82:$OPS_VERSION" images/php82 --progress=plain --push
+docker buildx build --platform=$PLATFORM -t "imarcagency/ops-apache-php72:$OPS_VERSION" images/php72 --progress=plain --push
+docker buildx build --platform=$PLATFORM -t "imarcagency/ops-apache-php73:$OPS_VERSION" images/php73 --progress=plain --push
+docker buildx build --platform=$PLATFORM -t "imarcagency/ops-apache-php74:$OPS_VERSION" images/php74 --progress=plain --push
+docker buildx build --platform=$PLATFORM -t "imarcagency/ops-apache-php80:$OPS_VERSION" images/php80 --progress=plain --push
+docker buildx build --platform=$PLATFORM -t "imarcagency/ops-apache-php81:$OPS_VERSION" images/php81 --progress=plain --push
+docker buildx build --platform=$PLATFORM -t "imarcagency/ops-apache-php82:$OPS_VERSION" images/php82 --progress=plain --push
 docker buildx build --platform=$PLATFORM -t "imarcagency/ops-apache-php83:$OPS_VERSION" images/php83 --progress=plain --push
-#docker buildx build --platform=$PLATFORM -t "imarcagency/ops-node:$OPS_VERSION" images/node --push
-#docker buildx build --platform=$PLATFORM -t "imarcagency/ops-utils:$OPS_VERSION" images/utils --push
+docker buildx build --platform=$PLATFORM -t "imarcagency/ops-node:$OPS_VERSION" images/node --push
+docker buildx build --platform=$PLATFORM -t "imarcagency/ops-utils:$OPS_VERSION" images/utils --push
 
 echo "Complete."
