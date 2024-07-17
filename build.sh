ab38b18e678f0c900106a782ebf4fc52b27f8c56
Kevin Hamer <kevin@imarc.com>
2024-07-17 09:47:43 -0400

    changing OPS_VERSION to be something that can be passed in

--- build.sh
+++ build.sh
@@ -1,11 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
-OPS_VERSION=$(jq -r .version package.json)
-echo -n $OPS_VERSION > VERSION
+if [ -z $OPS_VERSION ]; then
+    OPS_VERSION=$(jq -r .version package.json)
+fi
 
 PLATFORM="${PLATFORM-linux/amd64,linux/arm64,linux/arm64/v8}"
+
+echo "Building docker images for $PLATFORM and ops version $OPS_VERSION..."
+
+echo -n $OPS_VERSION > VERSION
 
 #docker buildx build --platform=$PLATFORM -t "imarcagency/ops-apache-php72:$OPS_VERSION" images/php72 --progress=plain --push
 #docker buildx build --platform=$PLATFORM -t "imarcagency/ops-apache-php73:$OPS_VERSION" images/php73 --progress=plain --push
@@ -16,3 +21,5 @@ PLATFORM="${PLATFORM-linux/amd64,linux/arm64,linux/arm64/v8}"
 docker buildx build --platform=$PLATFORM -t "imarcagency/ops-apache-php83:$OPS_VERSION" images/php83 --progress=plain --push
 #docker buildx build --platform=$PLATFORM -t "imarcagency/ops-node:$OPS_VERSION" images/node --push
 #docker buildx build --platform=$PLATFORM -t "imarcagency/ops-utils:$OPS_VERSION" images/utils --push
+
+echo "Complete."
